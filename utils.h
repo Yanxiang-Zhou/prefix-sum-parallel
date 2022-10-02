c9869a755a9ba3750cfc744f234322299b9665ee
Yanxiang Zhou <94133385+yanxiangZho@users.noreply.github.com>
2022-10-02 17:19:10 -0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -8,13 +8,6 @@
 #include <sstream>
 #include <unistd.h>
 
-/*
-    write_array_by_rank() prints data in the format below, ordered by rank.
-
-    rank {rank} {description}: {array[0]}, {array[1]}, ..., {array[array_len-1]}
-
-    Note: this is a collective operation, meaning all processes must call this function.
-*/
 void write_array_by_rank(MPI_Comm comm, const char* description, const int* array, int array_len);
 
 
